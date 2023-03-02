44a734cfd56e13c97c9bb0adb4ff2b145e46aeb0
JobaerAbdi <jobaerrahman757@gmail.com>
2023-03-02 23:04:06 +0600

    Card setup completed

--- js/main.js
+++ js/main.js
@@ -7,7 +7,28 @@ const loadFetchData = ()=>{
 };
 
 const loadDisplayData = (allData)=>{
+    const mainContainer = document.getElementById('card-container');
     allData.forEach(singleData=>{
+        const {image,features,name,published_in} = singleData;    // width="400" height="400"
         console.log(singleData);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card p-3">
+            <img class="img-fluid rounded" src="${image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Features</h5>
+                <div>
+                    <p>1. ${features[0]}</p>               
+                    <p>2. ${features[1]}</p>
+                    <p>3. ${features[2]}</p>
+                </div>
+            </div>
+            <hr/>
+            <div>
+            </div>
+        </div>
+        `;
+        mainContainer.appendChild(div);
     });
 };
